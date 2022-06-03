64f750955958c6be2d5df52314420559f5d453f5
AKaczus <b.lukasik@interia.pl>
2022-06-03 11:03:09 +0200

    Home page + more gallery things + visual update

--- weblogin/frontend/script.js
+++ weblogin/frontend/script.js
@@ -48,4 +48,20 @@ handle_button_click_signin = (event) => {
     fetch(url, options).then(console.log);
 }
 
+read_more  = (event) => {
+  var dots = document.getElementById("dots");
+  var moreText = document.getElementById("more");
+  var btnText = document.getElementById("myBtn");
+  if (dots.style.display === "none") {
+    dots.style.display = "inline";
+    btnText.innerHTML = "Read more";
+    moreText.style.display = "none";
+  } else {
+    dots.style.display = "none";
+    btnText.innerHTML = "Read less";
+    moreText.style.display = "inline";
+  }
+}
+
+
 
